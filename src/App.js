c95adf48e969c10c58062bb45e00a6425c66fb98
paul38 <pauls38@tut.by>
2022-09-21 22:01:35 +0300

    Added message feed and form functionality

--- src/App.js
+++ src/App.js
@@ -1,22 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
+
+const AUTHORS = {
+  ME: "ME",
+  BENDER_BOT: "BENDER_BOT",
+};
+
+const REPLY_MESSAGE = "Beep-boop-beep, kiss my shiny metal ass, human!";
+
+const FORM_ID = "message-form";
 
 function App() {
+  const [messages, setMessages] = useState([]);
+
+  // This hook will trigger on messages array changes
+  useEffect(() => {
+    if (
+      messages.length > 0 &&
+      messages[messages.length - 1].author === AUTHORS.ME
+    ) {
+      setTimeout(() => {
+        setMessages([
+          ...messages,
+          { text: REPLY_MESSAGE, author: AUTHORS.BENDER_BOT },
+        ]);
+      }, 1500);
+    }
+  }, [messages]);
+
+  // TODO: This method should be used as a submit function for a form
+  const onSubmitMessage = (event) => {
+    event.preventDefault()
+
+    const message = event.target[0].value;
+
+    if (message) {
+      document.getElementById(FORM_ID).reset();
+
+      setMessages([...messages, { text: message, author: AUTHORS.ME }]);
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Ах, у ели, ах у ёлки, ах у ели злые волки разбросали все иголки!
-        </a>
+        <div className="message-form">
+          {messages.map((item) => (
+            <div className="message">
+              <span>Author: {item.author}</span>
+              <br />
+              <text>{item.text}</text>
+            </div>
+          ))}
+          <form id={FORM_ID} className="message-form" onSubmit={onSubmitMessage}>
+            <input placeholder="Message..." id="message" />
+            <button type="submit">Submit</button>
+          </form>
+        </div>
       </header>
     </div>
   );
